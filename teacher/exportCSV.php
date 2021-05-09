e6859cc004e8d0b627ff272ced0b06eb610d5e55
Branislav <branislavprokop98@gmail.com>
2021-05-09 13:24:37 +0200

    export to csv

--- teacher/exportCSV.php
+++ teacher/exportCSV.php
@@ -1,14 +1,18 @@
 <?php
-session_start();
+require_once "../Controllers/TestGradingController.php";
 
 if (isset($_POST["type"]) && $_POST["type"] == "exportCSV") {
     $id = explode("-", $_POST["id"])[1];
-    $result = $_SESSION["students"];
+    $result = (new TestGradingController())->getStudentsPerTest($id);
     $f = fopen('php://output', 'a');
 
     foreach ($result as $item) {
-        $surname = explode(" ", $item["name"])[0];
-        $name = explode(" ", $item["name"])[1];
+        $surname = "";
+        $array = explode(" ", $item["name"]);
+        for($i = 0; $i < sizeof($array)-1; $i++){
+            $surname .= $array[$i];
+        }
+        $name = end($array);
         fputcsv($f, [$item["IDStudent"], $name, $surname, $item["score"]]);
     }
 
